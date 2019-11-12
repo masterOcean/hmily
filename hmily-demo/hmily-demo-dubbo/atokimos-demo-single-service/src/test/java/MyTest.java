b7004bc9fb64176fdf5dc92f60bf689d920c02b3
sea0523@163.com <sea0523@163.com>
2019-11-12 10:47:24 +0800

    修改代码

---
+++ hmily-demo/hmily-demo-dubbo/atokimos-demo-single-service/src/test/java/MyTest.java
@@ -0,0 +1,60 @@
+import java.math.BigDecimal;
+
+import org.dromara.hmily.demo.dubbo.order.AtokimoSingleServiceApplication;
+import org.dromara.hmily.demo.dubbo.order.service.OrderService;
+import org.dromara.hmily.demo.dubbo.order.service.PaymentService;
+import org.dromara.hmily.demo.dubbo.order.test.AspectTestService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=AtokimoSingleServiceApplication.class)
+//@ImportResource({"classpath:spring-dubbo.xml"})
+//@MapperScan("org.dromara.hmily.demo.dubbo.order.mapper")
+public class MyTest {
+	@Autowired
+	private AspectTestService testService;
+	
+	@Autowired
+	private OrderService orderService;
+	
+	@Test
+	public void test1() {
+		System.out.println(""+testService.getClass().toString());
+		testService.out();
+	}
+	
+	@Test
+	public void test2() {
+		Thread t1 = new Thread(new Runnable() {
+			
+			public void run() {
+				orderService.orderPay(1, new BigDecimal(10));
+				
+			}
+		}); 
+		t1.start();
+		Thread t2 = new Thread(new Runnable() {
+			
+			public void run() {
+				orderService.orderPay(1, new BigDecimal(10));
+				
+			}
+		}); 
+		t2.start();
+		try {
+ 			Thread.currentThread().sleep(100*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
